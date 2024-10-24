a457fe78600dcd1dc89ab8bfd8663c9627443f67
Raj Kumar <146169932+rajwhatsapp@users.noreply.github.com>
2024-10-24 22:05:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "enter your session",
-MONGODB: process.env.MONGODB || "enter mongodb url",
+SESSION_ID: process.env.SESSION_ID || "HjwX1YaI#RRGrPpX2Wp32UQK3Xun_yb9OgzPdSC1zucqDK1usqA0",
+MONGODB: process.env.MONGODB || "mongodb+srv://suhail:suhail@clustere.tuhzpez.mongodb.net/?retryWrites=true&w=majority",
 };
